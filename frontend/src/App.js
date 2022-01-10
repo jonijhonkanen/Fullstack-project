bfa207a0f875609cd1b950ced5422926f0989027
Joni Honkanen <joni.honkanen@tuni.fi>
2022-01-10 14:26:51 +0200

    Modify App.js file.
    Include the navigation bar with Navigation.js and React Router module.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+//Navigation bar
+import Navigation from './Navigation';
+//Two main views
+import AdminView from './AdminView';
+import StudentView from './StudentView';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Frontend</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Navigation />
+        </header>
+        <div className="content">
+          <Routes>
+            <Route path="/" exact element={<AdminView />} />
+            <Route path="student/*" element={<StudentView />} />
+            <Route path="*" element={<h1>Error with page url!</h1>} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
