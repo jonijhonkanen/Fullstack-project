5f84efa1a2c35f4ea94cfa890715297f3f7d5ccb
Joni Honkanen <joni.honkanen@tuni.fi>
2022-01-12 16:21:53 +0200

    Add word adding for frontend.
    Implement the necessary functions and UI for adding new words.

--- frontend/src/AdminView.js
+++ frontend/src/AdminView.js
@@ -7,20 +7,108 @@ function AdminView() {
   //For loading status
   const [isPending, setIsPending] = useState(false);
 
+  //For word pairs
+  const [englishWord, setEnglishWord] = useState('');
+  const [finnishWord, setFinnishWord] = useState('');
+
+  //For testing (view all word pairs in console)
   async function fetchAll() {
-    let hr = await fetch('/all');
-    let data = await hr.json();
+    try {
+      setIsPending(true);
+      let hr = await fetch('/all');
+      let data = await hr.json();
+      //Test if query works
+      console.log(data);
+    } catch (error) {
+      setMessage('Error with fetching data!');
+    } finally {
+      setMessage('');
+      setIsPending(false);
+    }
+    //let hr = await fetch('/all');
+    //let data = await hr.json();
     //Test if query works
-    console.log(data);
+    //console.log(data);
+  }
+
+  //Send the words
+  async function sendWords(newWords) {
+    console.log(newWords);
+
+    setIsPending(true);
+    fetch(`/addwords/`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newWords),
+    })
+      .then(() => {
+        setIsPending(false);
+      })
+      .catch((error) => {
+        setIsPending(false);
+        setMessage(error);
+      });
+
+    /*
+    let hr = await fetch(`/addwords/`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newWords),
+    });
+    */
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    //New word pair:
+    const newWords = {
+      english: englishWord,
+      finnish: finnishWord,
+    };
+
+    //console.log(newWords);
+    //Call for send function
+    sendWords(newWords);
   }
 
   return (
     <div className="admin_view">
       <h1>Admin</h1>
       <div className="message_field">
-        <div className="load-all">
-          <button onClick={fetchAll}> LATAA KAIKKI </button>
-        </div>
+        {message && <div className="message">{message}</div>}
+        {isPending && <div>Loading...</div>}
+      </div>
+      <div className="add_wordpair">
+        <h3>Add a word pair:</h3>
+        <form onSubmit={handleSubmit}>
+          <div className="write-description">
+            <span> English: </span>
+            <input
+              type="text"
+              onChange={(e) => setEnglishWord(e.target.value)}
+              required
+              value={englishWord}
+            />
+            <span> Finnish: </span>
+            <input
+              type="text"
+              onChange={(e) => setFinnishWord(e.target.value)}
+              required
+              value={finnishWord}
+            />
+            <button type="submit">Add words</button>
+          </div>
+        </form>
+      </div>
+      <div className="load-all">
+        <button onClick={fetchAll}> LOAD ALL </button>
       </div>
     </div>
   );
