e581379b8cf95fe60e21b8377f0781fab1bbd330
Joni Honkanen <joni.honkanen@tuni.fi>
2022-01-20 13:33:33 +0200

    Modify index.js file.
    Add routing path for index.html and define path for favicon.

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ const student = require('./routes/student.js');
 
 //Use of frontend (Admin as homepage)
 app.use(express.static('frontend/build'));
+//app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+//Favicon path (is this really necessary?)
+//app.use(favicon(__dirname + '/frontend/build/favicon.ico'));
+app.use('/favicon.ico', express.static('frontend/build/favicon.ico'));
 
 //Port definition (Heroku dedicated or default)
 const port = process.env.PORT || 8080;
@@ -27,12 +32,13 @@ app.use('/studentop', student);
 //For loading the index.html file (frontend)
 //res.sendFile(path.join(__dirname, 'path/to/your/index.html'), function (err)
 //path.join(__dirname, './frontend/build/index.html'),
-
+//path.join(__dirname, 'frontend', 'build', 'index.html'),
 //Retrieve UI content based on React Router components
 //Admin, Student, Error with URL!
-app.get('/*', function (req, res) {
+
+app.get('/*', (req, res) => {
   res.sendFile(
-    path.join(__dirname, 'frontend', 'build', 'index.html'),
+    path.join(__dirname + '/frontend/build/index.html'),
     function (err) {
       if (err) {
         res.status(500).send(err);
